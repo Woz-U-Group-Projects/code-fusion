51a9ad991d64e40093ac90e3345a9c5e6b9cd456
john <johnsantry82@gmail.com>
2019-04-20 09:46:04 -0700

    added shoe.js model to /models

---
+++ models/shoes.js
@@ -0,0 +1,93 @@
+import mongoose from 'mongoose';
+
+var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  sku: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  asin: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: false
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  shipWeight: {
+    type: Number,
+    required: true
+  },
+  shipWidth: {
+    type: Number,
+    required: true
+  }, 
+  shipHeight: {
+    type: Number,
+    required: true
+  }, 
+  shipDepth: {
+    type: Number,
+    required: true
+  },
+  priceList: {
+    type: Number,
+    required: true
+  },
+  priceRetail: {
+    type: Number,
+    required: true
+  },
+  priceSavings: {
+    type: Number,
+    required: false
+  },
+  percentSavings: {
+    type: Number,
+    required: false
+  },
+  priceWholesale: {
+    type: Number,
+    required: true
+  }
+});
+
+
+// UserSchema.pre("save", function(next) {
+//   let salt = bcrypt.genSaltSync(10);
+//   this.password = bcrypt.hashSync(this.password, salt);
+//   next();
+// });
+
+//mlab collection for shoes = "shoes"
+module.exports = mongoose.model("Shoes", UserSchema, "shoes");
